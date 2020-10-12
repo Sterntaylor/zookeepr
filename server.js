c9cb43af3487ff07e44e331a63856b60f4696e71
Taylor Stern <sterntaylor0629@gmail.com>
2020-10-11 19:44:45 -0700

    merged front-end

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const path = require('path');
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 //creating a route that front-end requests data from
 const { animals } = require('./data/animals');
 
@@ -93,6 +94,16 @@ app.get('/api/animals/:id', (req, res) => {
     res.send(404);
   }
 });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+//POST
 app.post('/api/animals', (req, res) => {
   // set id based on what the next index of the array will be
   req.body.id = animals.length.toString();
